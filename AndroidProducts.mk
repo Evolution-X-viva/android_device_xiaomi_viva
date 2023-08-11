3602b6750057954f64a7131d1e3353309d1e3dfa
Zan <122100768+RndwnZ@users.noreply.github.com>
2023-08-11 08:22:29 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,7 +6,7 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_viva.mk
+    $(LOCAL_DIR)/evolution_viva.mk
 
 COMMON_LUNCH_CHOICES := \
-    $(foreach FLAVOR, user userdebug eng, lineage_viva-$(FLAVOR))
+    $(foreach FLAVOR, user userdebug eng, evolution_viva-$(FLAVOR))
