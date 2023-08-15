1fc8dbbec860cfab3e857fa6b414a2cbc4a3d0f6
Zan <122100768+RndwnZ@users.noreply.github.com>
2023-08-15 14:03:59 +0700

    Cherish Bringup

--- evolution_viva.mk
+++ evolution_viva.mk
@@ -13,14 +13,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Inherit some common EvolutionX Stuff
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
-PRODUCT_NAME := evolution_viva
+PRODUCT_NAME := cherish_viva
 PRODUCT_DEVICE := viva
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 11 Pro 
-EVO_BUILD_TYPE := UNOFFICIAL
+
+# CherishOS Stuff with GApps
 EXTRA_UDFPS_ANIMATIONS := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
+WITH_GMS := true
+
+# Maintainer Stuff
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.cherish.maintainer=RndwnZan
+CHERISH_BUILD_TYPE := UNOFFICIAL
